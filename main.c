c6d13677027dd6822233c18ac3f2166a243022f0
user <user@localhost.localdomain>
2018-12-24 17:00:00 +0100

    mandel

--- main.c
+++ main.c
@@ -2,7 +2,8 @@
 
 t_complex min = {-1.5, -1};
 t_complex max = {0.7, 1};
-int			iter = 255;
+int			iter = 155;
+float zoom = 1;
 void		julia(t_env *env)
 {
 	t_point 	pos;
@@ -17,7 +18,7 @@ void		julia(t_env *env)
 		{
 			cur.r = toReal(pos, env, min.r, max.r);
 			cur.i = toComplex(pos, env, min.i, max.i);
-			n = calcMandel(cur, iter);
+			n = calcMandel(cur, 255);
 			put_pixel(env, new_point(pos.x, pos.y, 0, 0), RGB(255 % n, 255 % n, 255 % n));
 		}
 	}
@@ -27,6 +28,11 @@ int hook(int key, t_env *env)
 {
 	if (key == 61)
 	{
+		/*min.r += 0.1;
+		min.i += 0.1;*/
+		max.r -= 0.1;
+		max.i -= 0.1;
+		clear(env);
 		julia(env);
 		render(env);
 	}	
